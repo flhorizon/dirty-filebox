4b3a115e785ab5054cdcdb1abc423ad5376179c2
Florian Fouratier <ffourati@student.42.fr>
2018-06-09 19:32:15 +0200

    Mostly implemented backend

--- back/main.js
+++ back/main.js
@@ -1,11 +1,52 @@
 'use strict'
 
-const express = require('express')
-const compression = require('compression')
+const { Transform } = require('stream')
 
-const app = express()
-const router = express.Router()
+const destinationFolder = process.env["DIRTY_FILEBOX_DESTINATION"] || "/tmp"
 
-app.use(compression())
 
-app.use(express.static('../public'))
+class Base64Transform extends Transform {
+	constructor(options) {
+		super(options);
+	}
+	_transform(chunk, encoding, cb) {
+		cb(null, Buffer.from(chunk, 'base64'));
+	}
+
+}
+
+const multer  = require('multer')
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, destinationFolder)
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalName + '-' + Date.now())
+	}
+})
+const upload = multer({ storage: storage });
+
+
+const compression = require('compression');
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'), compression())
+
+app.put('/dump'
+		, require('decompress').create()
+		, function(req, res, next) {
+			req.pipe(new Base64Transform())
+            next()
+		}
+		, upload.single('givenFile')
+        , function (req, res, next) {
+			console.log(`[+] PUT ${req.file.originalName}`);
+			next();
+		})
+
+
+
+app.listen(8000)
+console.log("[i] Storing incoming files in ", destinationFolder)
